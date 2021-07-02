f37d0b857710af72e89a35da075273d26c5e16cc
High_Lander <codywmarkham@gmail.com>
2021-07-01 20:51:41 -0500

    pairing api genres

--- assets/js/lastfm.js
+++ assets/js/lastfm.js
@@ -1,31 +1,34 @@
-// <script type="text/javascript" src="lastfm.api.md5.js"></script> 
-// <script type="text/javascript" src="lastfm.api.js"></script> 
-// <script type="text/javascript" src="lastfm.api.cache.js"></script> 
-// <script type="text/javascript" src="json2.js"></script> 
+var RapHipHop
+var Indiefolk
+var Jazz
+var Reggae
+var ElectronicDance
+var Country
+var Blues
+var Classical
+var Rock
+var RNB
+var Alternative
 
 
 
 
 
-http://ws.audioscrobbler.com//2.0/?method=chart.gettopartists=rap&api_key=b2eec550419a940827175fdc3ac752b7
 
 
 
 
 
-/* Create a cache object */
-var cache = new LastFMCache();
 
-/* Create a LastFM object */
-var lastfm = new LastFM({
-  apiKey    : 'b2eec550419a940827175fdc3ac752b7',
-  apiSecret : 'aa5ac1c62091d25d601b6365b9844cd1',
-  cache     : cache
-});
 
-/* Load some artist info. */
-lastfm.artist.getInfo({artist: ''}, {success: function(data){
-  /* Use data. */
-}, error: function(code, message){
-  /* Show error message. */
-}});
+function pulltopartist(genre) {
+  top(genre)
+}
+
+  var queryURL =
+    "http://ws.audioscrobbler.com//2.0/?method=chart.gettopartists&page&limit=5&api_key=b2eec550419a940827175fdc3ac752b7&format=json&" + genre;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })

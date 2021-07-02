75cbbb77adc1ec5f3e50cc9e58dc2a91693483f4
zavaletaosv <osvzavaleta@gmail.com>
2021-07-01 21:07:38 -0500

    JS updated: queryURL updated, limit added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 function genreName(Name) {
 
     var napsterAPIKey = 'ZGE2ZTkzODgtZDc0Yy00MzdhLTk4Y2YtNDA4YjMzMTlkNjY3';
-    var queryURL = "https://api.napster.com/v2.2/"+ Name + "/g.397/artists/top?apikey=ZGE2ZTkzODgtZDc0Yy00MzdhLTk4Y2YtNDA4YjMzMTlkNjY3";
+    var queryURL = "https://api.napster.com/v2.2/"+ Name + "/g.397/artists/top?limit=5?apikey=ZGE2ZTkzODgtZDc0Yy00MzdhLTk4Y2YtNDA4YjMzMTlkNjY3";
 
     $.ajax({
         url: queryURL,

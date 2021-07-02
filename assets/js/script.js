1777118fca92f39da32c3760fb8ea467aa39dacd
zavaletaosv <osvzavaleta@gmail.com>
2021-07-01 19:48:50 -0500

    JS updated: ajax request created

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,10 @@ function genreName(Name) {
 
     var napsterAPIKey = 'ZGE2ZTkzODgtZDc0Yy00MzdhLTk4Y2YtNDA4YjMzMTlkNjY3';
     var queryURL = "https://api.napster.com/v2.1/genre/" + Name +"?apikey=ZGE2ZTkzODgtZDc0Yy00MzdhLTk4Y2YtNDA4YjMzMTlkNjY3";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    });
 }
 
